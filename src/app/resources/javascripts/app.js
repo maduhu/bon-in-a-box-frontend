234b2d7336163b292730e6089b1aff29581ea580
CAScgutierrez <cgutierrez@humboldt.org.co>
2015-09-11 11:54:49 -0500

    hexagon csss icon

--- src/app/resources/javascripts/app.js
+++ src/app/resources/javascripts/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var mn = $("nav");
-    mns = "nav_scrolled";
-    hdr = $('.titlebar').height();
+
 
 $(window).scroll(function() {
+    var mn = $("nav");
+    var mns = "nav_scrolled";
+    var hdr = $('.titlebar').height();
   if( $(this).scrollTop() > hdr ) {
     mn.addClass(mns);
   } else {
@@ -17,24 +18,24 @@ var actualimage = '';
     $(document).scroll(function() {
         var top = $(document).scrollTop();
         var bheight = 700;
-        var imgs = new Array();
+        var imgs = new Array[];
         imgs[0] = "/image/bg1.jpg";
         imgs[1] = "/image/bg2.jpg";
         imgs[2] = "/image/bg3.jpg";
         imgs[3] = "/image/bg4.jpg";
-        if (top >= bheight * 0 && top < bheight * 1 && actualimage != imgs[0]) {
+        if (top >= bheight * 0 && top < bheight * 1 && actualimage !== imgs[0]) {
             $('.backg').css('background-image', 'url(' + imgs[0] + ')');
                 actualimage = imgs[0];
         }
-        else if (top >= bheight * 1 && top < bheight * 2 && actualimage != imgs[1]) {
+        else if (top >= bheight * 1 && top < bheight * 2 && actualimage !== imgs[1]) {
             $('.backg').css('background-image', 'url(' + imgs[1] + ')');
             actualimage = imgs[1];
         }
-        else if (top >= bheight * 2 && top < bheight * 3 && actualimage != imgs[2]) {
+        else if (top >= bheight * 2 && top < bheight * 3 && actualimage !== imgs[2]) {
             $('.backg').css('background-image', 'url(' + imgs[2] + ')');
             actualimage = imgs[2];
         }
-        else if (top >= bheight * 3 && top < bheight * 4 && actualimage != imgs[3]) {
+        else if (top >= bheight * 3 && top < bheight * 4 && actualimage !== imgs[3]) {
             $('.backg').css('background-image', 'url(' + imgs[3] + ')');
             actualimage = imgs[3];
         }
