b4e95ffc3bd0d9d1b85b9ab0ac2920d998eb7520
CAScgutierrez <cgutierrez@humboldt.org.co>
2015-09-11 16:20:07 -0500

    navbar y filtros

--- src/app/resources/javascripts/app.js
+++ src/app/resources/javascripts/app.js
@@ -1,42 +1,40 @@
 'use strict';
 
+var height = document.getElementById('titlebar').offsetHeight;
 
+$(window).scroll(function()  {
 
-$(window).scroll(function() {
-    var mn = $("nav");
-    var mns = "nav_scrolled";
-    var hdr = $('.titlebar').height();
-  if( $(this).scrollTop() > hdr ) {
-    mn.addClass(mns);
+  if( $(this).scrollTop() > height ) {
+    $("#mainnav").addClass("nav_scrolled");
   } else {
-    mn.removeClass(mns);
+    $("#mainnav").removeClass("nav_scrolled");
   }
 });
 
 var actualimage = '';
 
-    $(document).scroll(function() {
-        var top = $(document).scrollTop();
-        var bheight = 700;
-        var imgs = new Array[];
-        imgs[0] = "/image/bg1.jpg";
-        imgs[1] = "/image/bg2.jpg";
-        imgs[2] = "/image/bg3.jpg";
-        imgs[3] = "/image/bg4.jpg";
-        if (top >= bheight * 0 && top < bheight * 1 && actualimage !== imgs[0]) {
-            $('.backg').css('background-image', 'url(' + imgs[0] + ')');
-                actualimage = imgs[0];
-        }
-        else if (top >= bheight * 1 && top < bheight * 2 && actualimage !== imgs[1]) {
-            $('.backg').css('background-image', 'url(' + imgs[1] + ')');
-            actualimage = imgs[1];
-        }
-        else if (top >= bheight * 2 && top < bheight * 3 && actualimage !== imgs[2]) {
-            $('.backg').css('background-image', 'url(' + imgs[2] + ')');
-            actualimage = imgs[2];
-        }
-        else if (top >= bheight * 3 && top < bheight * 4 && actualimage !== imgs[3]) {
-            $('.backg').css('background-image', 'url(' + imgs[3] + ')');
-            actualimage = imgs[3];
-        }
-    });
+$(document).scroll(function() {
+    var top = $(document).scrollTop();
+    var bheight = 500;
+    var imgs = new Array();
+        imgs[0] = "/images/bg1.jpg";
+        imgs[1] = "/images/bg2.jpg";
+        imgs[2] = "/images/bg3.jpg";
+        imgs[3] = "/images/bg4.jpg";
+    if (top >= bheight * 0 && top < bheight * 1 && actualimage !== imgs[0]) {
+        $('.backg').css('background-image', 'url(' + imgs[0] + ')');
+            actualimage = imgs[0];
+    }
+    else if (top >= bheight * 1 && top < bheight * 2 && actualimage !== imgs[1]) {
+        $('.backg').css('background-image', 'url(' + imgs[1] + ')');
+        actualimage = imgs[1];
+    }
+    else if (top >= bheight * 2 && top < bheight * 3 && actualimage !== imgs[2]) {
+        $('.backg').css('background-image', 'url(' + imgs[2] + ')');
+        actualimage = imgs[2];
+    }
+    else if (top >= bheight * 3 && top < bheight * 4 && actualimage !== imgs[3]) {
+        $('.backg').css('background-image', 'url(' + imgs[3] + ')');
+        actualimage = imgs[3];
+    }
+});
