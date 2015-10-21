eef1355af113547620ac9b52213282c1f41d200e
Valentina Grajales Olarte <valegrajales@gmail.com>
2015-10-20 21:50:13 -0500

    Improve admin dashboard appearance

--- src/app/resources/javascripts/admin/controllers/adminControllers.js
+++ src/app/resources/javascripts/admin/controllers/adminControllers.js
@@ -3,7 +3,7 @@
 angular.module('adminConsole')
 	.controller('adminConsoleCtrl', function($timeout, $state, growlService){
 		//Welcome Message
-		growlService.growl('Welcome to box in a box!', 'inverse');
+		growlService.growl('Welcome to box in a box admin dashboard!', 'inverse');
 
 		// By default Sidbars are hidden in boxed layout and in wide layout only the right sidebar is hidden.
 		this.sidebarToggle = {
@@ -12,7 +12,8 @@ angular.module('adminConsole')
 		};
 
 		// By default template has a boxed layout
-		this.layoutType = localStorage.getItem('ma-layout-status');
+		//this.layoutType = localStorage.getItem('ma-layout-status');
+		this.layoutType = '1';
 
 		// For Mainmenu Active Class
 		this.$state = $state;
@@ -46,66 +47,12 @@ angular.module('adminConsole')
 	// Header
 	// =========================================================================
 	.controller('headerCtrl', function($timeout, messageService){
-		// Top Search
-		this.openSearch = function(){
-			angular.element('#header').addClass('search-toggled');
-		};
-
-		this.closeSearch = function(){
-			angular.element('#header').removeClass('search-toggled');
-		};
-
 		// Get messages and notification for header
 		this.img = messageService.img;
 		this.user = messageService.user;
 		this.user = messageService.text;
 
 		this.messageResult = messageService.getMessage(this.img, this.user, this.text);
-
-		//Clear Notification
-		this.clearNotification = function($event) {
-			$event.preventDefault();
-
-			var x = angular.element($event.target).closest('.listview');
-			var y = x.find('.lv-item');
-			var z = y.size();
-
-			angular.element($event.target).parent().fadeOut();
-
-			x.find('.list-group').prepend('<i class="grid-loading hide-it"></i>');
-			x.find('.grid-loading').fadeIn(1500);
-			var w = 0;
-
-			y.each(function(){
-				var z = $(this);
-				$timeout(function(){
-					z.addClass('animated fadeOutRightBig').delay(1000).queue(function(){
-						z.remove();
-					});
-				}, w+=150);
-			});
-
-			$timeout(function(){
-				angular.element('#notifications').addClass('empty');
-			}, (z*150)+200);
-		};
-
-		// Clear Local Storage
-		this.clearLocalStorage = function() {
-			//Get confirmation, if confirmed clear the localStorage
-			swal({
-				title: "Are you sure?",
-				text: "All your saved localStorage values will be removed",
-				type: "warning",
-				showCancelButton: true,
-				confirmButtonColor: "#F44336",
-				confirmButtonText: "Yes, delete it!",
-				closeOnConfirm: false
-			}, function(){
-				localStorage.clear();
-				swal("Done!", "localStorage is cleared", "success");
-			});
-		};
 
 		//Fullscreen View
 		this.fullScreen = function() {
