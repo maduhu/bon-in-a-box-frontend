e3418ae0b4815a5e8b466465eaca1f8b7179fecc
Valentina Grajales Olarte <valegrajales@gmail.com>
2015-11-05 00:01:36 -0500

    Show catgories icons in home page

--- src/app/resources/javascripts/directives.js
+++ src/app/resources/javascripts/directives.js
@@ -40,17 +40,38 @@ angular.module('bonInABoxHome')
 							'</div>' +
 							'<p>{{tool.shortDescription.english}}</p>' +
 							'<ul>' +
-								'<li>' +
-									'<div class="icon-ico-06"></div>' +
-								'</li>' +
-								'<li>' +
-									'<div class="icon-ico-07"></div>' +
-								'</li>' +
-								'<li>' +
-									'<div class="icon-ico-08"></div>' +
-								'</li>' +
-								'<li>' +
-									'<div class="icon-ico-09"></div>' +
+								'<li data-ng-repeat="category in tool.categories">' +
+									'<div class="icon-ico-01" data-ng-show="category === \'Observation Design\'" uib-tooltip="Observation Design" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-02" data-ng-show="category === \'Data collection\'" uib-tooltip="Data collection" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-03" data-ng-show="category === \'Data Management\'" uib-tooltip="Data Management" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-04" data-ng-show="category === \'Data analysis\'" uib-tooltip="Data analysis" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-05" data-ng-show="category === \'Report results\'" uib-tooltip="Report results" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-06" data-ng-show="category === \'Communities composition\'" uib-tooltip="Communities composition" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-07" data-ng-show="category === \'Ecosystem function\'" uib-tooltip="Ecosystem function" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-08" data-ng-show="category === \'Ecosystem structure\'" uib-tooltip="Ecosystem structure" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-09" data-ng-show="category === \'Genetic composition\'" uib-tooltip="Genetic composition" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-10" data-ng-show="category === \'Species populations\'" uib-tooltip="Species populations" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-11" data-ng-show="category === \'Species traits\'" uib-tooltip="Species traits" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-12" data-ng-show="category === \'Ecosystem services\'" uib-tooltip="Ecosystem services" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-13" data-ng-show="category === \'Citizen science\'" uib-tooltip="Citizen science" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-14" data-ng-show="category === \'Traditional knowledge\'" uib-tooltip="Traditional knowledge" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-15" data-ng-show="category === \'Invasive species\'" uib-tooltip="Invasive species" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-16" data-ng-show="category === \'CITES\'" uib-tooltip="CITES" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-17" data-ng-show="category === \'Migratory\'" uib-tooltip="Migratory" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-18" data-ng-show="category === \'Fresh water\'" uib-tooltip="Fresh water" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-19" data-ng-show="category === \'Marine\'" uib-tooltip="Marine" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-20" data-ng-show="category === \'Terrestrial\'" uib-tooltip="Terrestrial" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-21" data-ng-show="category === \'Dataset\'" uib-tooltip="Dataset" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-22" data-ng-show="category === \'Software\'" uib-tooltip="Software" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-23" data-ng-show="category === \'Data standards\'" uib-tooltip="Data standards" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-24" data-ng-show="category === \'Literature\'" uib-tooltip="Literature" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-25" data-ng-show="category === \'Amphibians\'" uib-tooltip="Amphibians" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-26" data-ng-show="category === \'Birds\'" uib-tooltip="Birds" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-27" data-ng-show="category === \'Invertebrates\'" uib-tooltip="Invertebrates" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-28" data-ng-show="category === \'Mammals\'" uib-tooltip="Mammals" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-29" data-ng-show="category === \'Fish\'" uib-tooltip="Fish" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-30" data-ng-show="category === \'Reptiles\'" uib-tooltip="Reptiles" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
+									'<div class="icon-ico-31" data-ng-show="category === \'Plants\'" uib-tooltip="Plants" tooltip-placement="bottom" tooltip-trigger="mouseenter"></div>' +
 								'</li>' +
 							'</ul>' +
 							'<button class="ver_mas right" data-toggle="tooltip" title="more" href="")>+</button>' +
